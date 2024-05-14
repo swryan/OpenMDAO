5ed1ad916a8e2e066d6bb6dda1c10f5d788d1d24
swryan <swryan@users.noreply.github.com>
2024-05-14 13:18:12 -0400

    install petsc 1st on latest workflow

--- .github/scripts/install_latest_deps.sh
+++ .github/scripts/install_latest_deps.sh
@@ -26,61 +26,6 @@ echo "NUMPY_VER=$NUMPY_VER" >> $GITHUB_ENV
 echo "SCIPY_VER=$SCIPY_VER" >> $GITHUB_ENV
 
 echo "============================================================="
-echo "Install latest versions of 'required' dependencies"
-echo "============================================================="
-python -m pip install --upgrade --pre networkx
-python -m pip install --upgrade --pre requests
-python -m pip install --upgrade --pre packaging
-
-echo "============================================================="
-echo "Install latest versions of 'docs' dependencies"
-echo "============================================================="
-python -m pip install --upgrade --pre matplotlib
-python -m pip install --upgrade --pre numpydoc
-python -m pip install --upgrade --pre jupyter-book
-python -m pip install --upgrade --pre sphinx-sitemap
-python -m pip install --upgrade --pre ipyparallel
-
-echo "============================================================="
-echo "Install latest versions of 'doe' dependencies"
-echo "============================================================="
-python -m pip install --upgrade --pre pyDOE3
-
-echo "============================================================="
-echo "Install latest versions of 'notebooks' dependencies"
-echo "============================================================="
-python -m pip install --upgrade --pre notebook
-python -m pip install --upgrade --pre ipympl
-
-echo "============================================================="
-echo "Install latest versions of 'visualization' dependencies"
-echo "============================================================="
-python -m pip install --upgrade --pre bokeh
-python -m pip install --upgrade --pre colorama
-
-echo "============================================================="
-echo "Install latest versions of 'test' dependencies"
-echo "============================================================="
-python -m pip install --upgrade --pre parameterized
-python -m pip install --upgrade --pre numpydoc
-python -m pip install --upgrade --pre pycodestyle
-python -m pip install --upgrade --pre pydocstyle
-python -m pip install --upgrade --pre testflo
-python -m pip install --upgrade --pre websockets
-python -m pip install --upgrade --pre aiounittest
-echo "the latest version of playwright (1.38.0) is pinned to"
-echo "greenlet 2.0.2 which does not build properly for python 3.12"
-echo "https://github.com/microsoft/playwright-python/issues/2096"
-python -m pip install --upgrade --pre playwright  || echo "Skipping playwright, no GUI testing!"
-python -m pip install --upgrade --pre num2words
-
-echo "============================================================="
-echo "Install latest versions of other optional packages"
-echo "============================================================="
-python -m pip install --upgrade --pre pyparsing psutil objgraph pyxdsm
-python -m pip install --upgrade --pre jax jaxlib
-
-echo "============================================================="
 echo "Install latest PETSc"
 echo "============================================================="
 conda install mpi4py petsc!=3.21.1 petsc4py -q -y
@@ -104,6 +49,63 @@ mpirun -n 3 python -c "import numpy; from mpi4py import MPI; comm = MPI.COMM_WOR
                        x = petsc4py.PETSc.Vec().createWithArray(numpy.ones(5)*comm.rank, comm=comm);  \
                        print(x.getArray())"
 echo "-----------------------"
+
+echo "============================================================="
+echo "Install latest versions of 'required' dependencies"
+echo "============================================================="
+python -m pip install --upgrade --pre networkx
+python -m pip install --upgrade --pre requests
+python -m pip install --upgrade --pre packaging
+
+echo "============================================================="
+echo "Install latest versions of 'docs' dependencies"
+echo "============================================================="
+python -m pip install --upgrade --pre matplotlib
+python -m pip install --upgrade --pre numpydoc
+python -m pip install --upgrade --pre jupyter-book
+python -m pip install --upgrade --pre sphinx-sitemap
+python -m pip install --upgrade --pre ipyparallel
+
+echo "============================================================="
+echo "Install latest versions of 'doe' dependencies"
+echo "============================================================="
+python -m pip install --upgrade --pre pyDOE3
+
+echo "============================================================="
+echo "Install latest versions of 'jax' dependencies"
+echo "============================================================="
+python -m pip install --upgrade --pre jax jaxlib
+
+echo "============================================================="
+echo "Install latest versions of 'notebooks' dependencies"
+echo "============================================================="
+python -m pip install --upgrade --pre notebook
+python -m pip install --upgrade --pre ipympl
+
+echo "============================================================="
+echo "Install latest versions of 'visualization' dependencies"
+echo "============================================================="
+python -m pip install --upgrade --pre bokeh
+python -m pip install --upgrade --pre colorama
+
+echo "============================================================="
+echo "Install latest versions of 'test' dependencies"
+echo "============================================================="
+python -m pip install --upgrade --pre parameterized
+python -m pip install --upgrade --pre numpydoc
+python -m pip install --upgrade --pre pycodestyle
+python -m pip install --upgrade --pre pydocstyle
+python -m pip install --upgrade --pre testflo
+python -m pip install --upgrade --pre websockets
+python -m pip install --upgrade --pre aiounittest
+python -m pip install --upgrade --pre playwright
+python -m pip install --upgrade --pre num2words
+
+echo "============================================================="
+echo "Install latest versions of other optional packages"
+echo "============================================================="
+python -m pip install --upgrade --pre pyparsing psutil objgraph pyxdsm
+python -m pip install --upgrade --pre jax jaxlib
 
 echo "============================================================="
 echo "Display conda environment"
