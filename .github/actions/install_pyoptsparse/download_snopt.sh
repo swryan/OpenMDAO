b7c6142a9c2bcd83049c859ddb2369a6cec7175f
swryan <swryan@users.noreply.github.com>
2025-09-30 14:22:19 -0400

    rmls

--- .github/actions/install_pyoptsparse/download_snopt.sh
+++ .github/actions/install_pyoptsparse/download_snopt.sh
@@ -22,7 +22,6 @@ if [[ "$SNOPT_VERSION" == "7.2" ]]; then
         echo "Downloading SNOPT version $SNOPT_VERSION from SNOPT_LOCATION_72 ..."
         mkdir SNOPT
         scp -qr $SNOPT_LOCATION_72 SNOPT
-        ls -R SNOPT
     else
         echo "SNOPT location not found for SNOPT version $SNOPT_VERSION, skipping download."
     fi
@@ -31,11 +30,9 @@ else
         echo "Downloading SNOPT version $SNOPT_VERSION from SNOPT_LOCATION_77 ..."
         mkdir SNOPT
         scp -qr $SNOPT_LOCATION_77 SNOPT
-        ls -R SNOPT
     elif [[ -n "$SNOPT_LOCATION" ]]; then
         echo "Downloading SNOPT version $SNOPT_VERSION from SNOPT_LOCATION ..."
         scp -qr $SNOPT_LOCATION .
-        ls SNOPT
     else
         echo "SNOPT location not found for SNOPT version $SNOPT_VERSION, skipping download."
     fi
